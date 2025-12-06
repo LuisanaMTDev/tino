c41daf50d9e900610110cf669f0faa1042729518
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2025-12-06 14:40:31 -0400

    add config_file field to App struct and order its fields

--- src/ratatui_app/app_and_rust_traits_impls.rs
+++ src/ratatui_app/app_and_rust_traits_impls.rs
@@ -1,12 +1,15 @@
 use ratatui::widgets::ListState;
 use tui_input::Input;
+
+use crate::app::config_file::ConfigFile;
 /// The main application which holds the state and logic of the application.
 #[derive(Debug)]
 pub struct App {
     /// Is the application running?
     pub running: bool,
-    pub file_name_input: Input,
     pub active_field: usize,
+    pub config_file: ConfigFile,
+    pub file_name_input: Input,
     pub type_items: Vec<String>,
     pub type_state: ListState,
     pub category_items: Vec<String>,
